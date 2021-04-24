ca6d3450f39a8312d024edc81df0cf41ef002cf6
pihanya <gostevvm@yandex.ru>
2021-04-25 01:13:02 +0300

    Apply WebStorm code style of JavaScript

--- desktop-app/main.js
+++ desktop-app/main.js
@@ -1,28 +1,29 @@
-const { app, BrowserWindow, protocol } = require('electron')
+const {app, BrowserWindow, protocol} = require('electron')
 const path = require('path')
 const console = require('console');
 if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '.').replace(/\\/g, '\\\\')
+    global.__static = require('path').join(__dirname, '.').replace(/\\/g, '\\\\')
 }
 
 try {
-  require('electron-reloader')(module)
-} catch (_) {}
+    require('electron-reloader')(module)
+} catch (_) {
+}
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration: true,
-      contextIsolation: false,
-      enableRemoteModule: true,
-      webSecurity: false,
-    }
-  })
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+            webSecurity: false,
+        }
+    })
 
-  win.loadFile('index.html')
+    win.loadFile('index.html')
 }
 
 const input = {value: "Node.JS"};
@@ -45,43 +46,44 @@ function sendToPython() {
         console.log(`child process exited with code ${code}`);
     });
 }
+
 sendToPython();
 
 // And this anywhere:
-function registerLocalVideoProtocol () {
-  protocol.registerFileProtocol('local-video', (request, callback) => {
-    const url = request.url.replace(/^local-video:\/\//, '')
-    // Decode URL to prevent errors when loading filenames with UTF-8 chars or chars like "#"
-    const decodedUrl = decodeURI(url) // Needed in case URL contains spaces
-    try {
-      // eslint-disable-next-line no-undef
-      return callback(path.join(__static, decodedUrl))
-    } catch (error) {
-      console.error(
-        'ERROR: registerLocalVideoProtocol: Could not get file path:',
-        error
-      )
-    }
-  })
+function registerLocalVideoProtocol() {
+    protocol.registerFileProtocol('local-video', (request, callback) => {
+        const url = request.url.replace(/^local-video:\/\//, '')
+        // Decode URL to prevent errors when loading filenames with UTF-8 chars or chars like "#"
+        const decodedUrl = decodeURI(url) // Needed in case URL contains spaces
+        try {
+            // eslint-disable-next-line no-undef
+            return callback(path.join(__static, decodedUrl))
+        } catch (error) {
+            console.error(
+                'ERROR: registerLocalVideoProtocol: Could not get file path:',
+                error
+            )
+        }
+    })
 }
 
 app.on('ready', async () => {
-  console.log("HI")
-  registerLocalVideoProtocol()
+    console.log("HI")
+    registerLocalVideoProtocol()
 })
 
 app.whenReady().then(() => {
-  createWindow();
+    createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
 })
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
--- desktop-app/preloadAdd.js
+++ desktop-app/preloadAdd.js
@@ -1,32 +1,31 @@
 window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+    const replaceText = (selector, text) => {
+        const element = document.getElementById(selector)
+        if (element) element.innerText = text
+    }
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
+    for (const type of ['chrome', 'node', 'electron']) {
+        replaceText(`${type}-version`, process.versions[type])
+    }
 
-  const fs = require('fs');
-  const path = require('path');
+    const fs = require('fs');
+    const path = require('path');
 
-  let rawdata = fs.readFileSync(path.resolve(__dirname, 'templates.json'));
-  let videos = JSON.parse(rawdata);
-  let videoLenWithOffset = videos.length;
-  for (var raw = 0; raw < videoLenWithOffset / 3; raw++) {
-    console.log("ROW")
-    $('#container' ).append(
-        `
+    let rawdata = fs.readFileSync(path.resolve(__dirname, 'templates.json'));
+    let videos = JSON.parse(rawdata);
+    let videoLenWithOffset = videos.length;
+    for (let raw = 0; raw < videoLenWithOffset / 3; raw++) {
+        console.log("ROW")
+        $('#container').append(`
           <div id="row${raw}" class="row" style="display: flex;align-items: center;">
           </div>
         `)
 
-    let idx = raw * 3
-    for (; idx < videoLenWithOffset && idx < (raw + 1) * 3; idx ++) {
-      let video = videos[idx]
-      $(`#row${raw}`).append(
-        `
+        let idx = raw * 3
+        for (; idx < videoLenWithOffset && idx < (raw + 1) * 3; idx++) {
+            let video = videos[idx]
+            $(`#row${raw}`).append(
+                `
         <div class="col">
           <button onclick="changeAction(${idx}, ${video.src}); " style="width: 200px;" class="btn btn-primary" type="button" id="dropdownMenuButton${idx}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             "Выбрать"
@@ -37,17 +36,17 @@ window.addEventListener('DOMContentLoaded', () => {
           </video>
         </div>
         `
-      ); 
-      console.log(video);
-    }
+            );
+            console.log(video);
+        }
 
-    for (; idx < (raw + 1) * 3; idx ++) {
-      $(`#row${raw}`).append(
-        `
+        for (; idx < (raw + 1) * 3; idx++) {
+            $(`#row${raw}`).append(
+                `
         <div class="col">
         </div>
         `
-      ); 
+            );
+        }
     }
-  }
 })
