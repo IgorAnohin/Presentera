8bd35a104e99f22bed6a4032a38a3caa69f49f1a
Igor Anokhin <ianokhin@croc.ru>
2021-04-24 20:12:17 +0300

    UI changes

---
+++ desktop-app/preloadAdd.js
@@ -0,0 +1,53 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+
+  const fs = require('fs');
+  const path = require('path');
+
+  let rawdata = fs.readFileSync(path.resolve(__dirname, 'templates.json'));
+  let videos = JSON.parse(rawdata);
+  let videoLenWithOffset = videos.length;
+  for (var raw = 0; raw < videoLenWithOffset / 3; raw++) {
+    console.log("ROW")
+    $('#container' ).append(
+        `
+          <div id="row${raw}" class="row" style="display: flex;align-items: center;">
+          </div>
+        `)
+
+    let idx = raw * 3
+    for (; idx < videoLenWithOffset && idx < (raw + 1) * 3; idx ++) {
+      let video = videos[idx]
+      $(`#row${raw}`).append(
+        `
+        <div class="col">
+          <button onclick="changeAction(${idx}, 'IMG_4315.MOV.mp4'); " style="width: 200px;" class="btn btn-primary" type="button" id="dropdownMenuButton${idx}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            "Выбрать"
+          </button>
+          <div style="height: 5px;"></div>
+          <video controls autoplay width="200px" height="360px">
+            <source src="local-video://IMG_4315.MOV.mp4" type="video/mp4"> -->
+          </video>
+        </div>
+        `
+      ); 
+      console.log(video);
+    }
+
+    for (; idx < (raw + 1) * 3; idx ++) {
+      $(`#row${raw}`).append(
+        `
+        <div class="col">
+        </div>
+        `
+      ); 
+    }
+  }
+})
